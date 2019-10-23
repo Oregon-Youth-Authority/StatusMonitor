eea81aa62e12f79d51028c59288d3d1664caa4a9
Curt Mullin <mullinc@oya.state.or.us>
2019-10-23 10:57:44 -0700

    init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IpDump.Controllers
+{
+    [Route("Home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+       public string Get()
+       {
+          return Request.HttpContext.Connection.RemoteIpAddress.ToString();
+       }
+    }
+}
