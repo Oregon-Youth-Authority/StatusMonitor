30321a2fe2f3f72a940d30481bbbae6f2cddff9d
Curt Mullin <curt.mullin@state.or.us>
2019-10-28 14:59:54 -0700

    use a simple apikey authn

--- StatusMonitor.Server/Startup.cs
+++ StatusMonitor.Server/Startup.cs
@@ -1,15 +1,23 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 using ApplicationStatusMonitor.Controllers;
+using AspNet.Security.ApiKey.Providers;
+using AspNet.Security.ApiKey.Providers.Events;
+using AspNet.Security.ApiKey.Providers.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -18,12 +26,17 @@ using NSwag;
 
 namespace ApplicationStatusMonitor
 {
+
    public class Startup
    {
       public Startup(IConfiguration configuration)
       {
          Configuration = configuration;
+         ValidKeys = ( Configuration["ApiKeySettings:ApiKeys"] ?? throw new Exception("Config value for ApiKeySettings:ApiKeys is missing"))
+            .Split(",").ToArray();
       }
+
+      public string[] ValidKeys { get; set; }
 
       public IConfiguration Configuration { get; }
 
@@ -42,12 +55,44 @@ namespace ApplicationStatusMonitor
          });
 
          services.AddControllers();
-         
          services.Configure<StatusMonitorDatabaseSettings>( Configuration.GetSection(nameof(StatusMonitorDatabaseSettings)));
          services.AddSingleton<IStatusMonitorDatabaseSettings>( sp => sp.GetRequiredService<IOptions<StatusMonitorDatabaseSettings>>().Value);
-
+         
          services.AddTransient<IStatusRepository<StatusMonitorReply>, StatusMonitorMongoRepository<StatusMonitorReply>>();
          services.AddTransient<IMonitorConfigurationRepository<MonitorConfiguration>, MonitorConfigurationMongoRepository<MonitorConfiguration>>();
+         
+         services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = ApiKeyDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = ApiKeyDefaults.AuthenticationScheme;
+                })
+                .AddApiKey(options =>
+                {
+                    options.Header = "X-API-KEY";
+                    options.HeaderKey = String.Empty;
+                    options.Events = new ApiKeyEvents
+                    {
+                        OnAuthenticationFailed = context =>
+                        {
+                            var ex = context.Exception;
+
+                            Trace.TraceError(ex.Message);
+
+                            context.Fail(ex);
+
+                            return Task.CompletedTask;
+                        },
+                        OnApiKeyValidated = context =>
+                        {
+                           if (!ValidKeys.Contains(context.ApiKey))
+                              return Task.CompletedTask;
+
+                           context.Success();
+
+                           return Task.CompletedTask;
+                        }
+                    };
+                });
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,14 +105,13 @@ namespace ApplicationStatusMonitor
 
          //app.UseHttpsRedirection();
 
-         // Enable middleware to serve generated Swagger as a JSON endpoint.
          app.UseOpenApi();
          app.UseSwaggerUi3();
 
          app.UseRouting();
 
-         //app.UseAuthorization();
-
+         app.UseAuthentication();
+         app.UseAuthorization();
          
          app.UseEndpoints(endpoints =>
          {
