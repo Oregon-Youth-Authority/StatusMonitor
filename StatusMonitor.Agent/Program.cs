d40e5118c7259865607517959cd85c600b03410b
Jeff Wobbe <wobbej@oya.state.or.us>
2019-10-29 07:17:48 -0700

    run as windows service

--- StatusMonitor.Agent/Program.cs
+++ StatusMonitor.Agent/Program.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using State.Or.Oya.Jjis.StatusMonitor.BackgroundServices;
+using State.Or.Oya.Jjis.StatusMonitor.Configuration;
 using State.Or.Oya.Jjis.StatusMonitor.Util;
+using State.Or.Oya.Jjis.StatusMonitor.WindowsServices;
 using State.Or.Oya.StatusMonitor.Client.Generated;
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
-using State.Or.Oya.Jjis.StatusMonitor.Configuration;
 
 namespace State.Or.Oya.Jjis.StatusMonitor
 {
@@ -16,21 +19,29 @@ namespace State.Or.Oya.Jjis.StatusMonitor
    {
       private static async Task Main(string[] args)
       {
+         var isService = !(Debugger.IsAttached || args.All(arg => arg != "--service"));
+         var config = GetConfiguration();
+
+         var hostBuilder = new HostBuilder().ConfigureServices((context, services) => services.ConfigureServices(config));
+         if (isService)
+            await hostBuilder.RunAsServiceAsync();
+         else
+            await hostBuilder.RunConsoleAsync();
+
+         Console.WriteLine("Application Exiting... ");
+      }
+
+      internal static IConfiguration GetConfiguration()
+      {
          var configFileName = "appsettings.json";
          var configFilePath = Path.Combine(Environment.CurrentDirectory, configFileName);
          configFilePath = File.Exists(configFilePath)
             ? configFilePath
             : configFileName;
 
-         var config = new ConfigurationBuilder()
+         return new ConfigurationBuilder()
             .AddJsonFile(configFilePath, false, true)
             .Build();
-
-         await new HostBuilder()
-            .ConfigureServices((context, services) => services.ConfigureServices(config))
-            .RunConsoleAsync();
-
-         Console.WriteLine("Application Exiting... ");
       }
    }
 
@@ -46,6 +57,11 @@ namespace State.Or.Oya.Jjis.StatusMonitor
                builder.AddConsole();
                builder.SetMinimumLevel(LogLevel.Information);
             })
+            //.AddLogging(builder =>
+            //{
+            //   builder.AddEventLog();
+            //   builder.SetMinimumLevel(LogLevel.Warning);
+            //})
             .AddTransient<ApplicationConfiguration>()
             .AddTransient(sp => config)
             .AddSingleton<INetworkUtil, NetworkUtil>()
