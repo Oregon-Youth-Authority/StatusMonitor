10657f02a83d54a236dec6dae857ad9d1679c084
Jeff Wobbe <wobbej@oya.state.or.us>
2019-11-05 13:47:25 -0800

    Present times in PST

---
+++ StatusMonitor.Server/Extensions/DateTimeExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApplicationStatusMonitor.Extensions
+{
+   public static class DateTimeExtension
+   {
+      public static DateTime ToPacificStandardTime(this DateTime dateTime)
+      {
+         return TimeZoneInfo.ConvertTime(dateTime, TimeZoneInfo.FindSystemTimeZoneById("America/Los_Angeles"));
+      }
+   }
+}
