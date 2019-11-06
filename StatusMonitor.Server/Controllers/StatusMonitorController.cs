cd97ec9cd033d2eda7402f97f1d3cf9b750956f7
jwobbeoya <jeff.wobbe.oya@gmail.com>
2019-11-06 13:24:42 -0800

    Data maintenance (#28)
    * Present times in PST
    
    * Fixed issue with update
    
    * Fixed issue with update
    
    * Data maitenance
    
    * favicon

--- StatusMonitor.Server/Controllers/StatusMonitorController.cs
+++ StatusMonitor.Server/Controllers/StatusMonitorController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using ApplicationStatusMonitor.Abstractions;
 
 namespace ApplicationStatusMonitor.Controllers
 {
