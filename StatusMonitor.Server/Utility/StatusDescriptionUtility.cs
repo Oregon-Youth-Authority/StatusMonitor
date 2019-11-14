4a06dfc73d6a8774f5af39d0501c327fbb4130ad
Jeff Wobbe <wobbej@oya.state.or.us>
2019-11-14 12:29:36 -0800

    Added status tooltips

---
+++ StatusMonitor.Server/Utility/StatusDescriptionUtility.cs
@@ -0,0 +1,18 @@
+﻿namespace ApplicationStatusMonitor.Utility
+{
+   public static class StatusDescriptionUtility
+   {
+      public static string GetDescription(string status)
+      {
+         return status switch
+         {
+            "Up" => "This status indicates that the agent is running and able to connect.",
+            "Down" => "This status indicates that the agent is running, but unable to connect.",
+            "Slow" => "This status indicates that the agent is running and able to connect, but the operation took longer than expected.",
+            "Timeout" => "This status indicates that the agent is running, but the connection attempt timed out.",
+            "Offline" => "This status indicates that the agent is either not running or unable to reach the API on on this site.",
+            _ => null
+         };
+      }
+   }
+}
