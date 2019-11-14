da931e6773de13c6b477805d6bdceb58c51c9a36
Jeff Wobbe <wobbej@oya.state.or.us>
2019-11-14 15:24:16 -0800

    Improved duration display

--- StatusMonitor.Server/Utility/DurationUtility.cs
+++ StatusMonitor.Server/Utility/DurationUtility.cs
@@ -7,11 +7,19 @@ namespace ApplicationStatusMonitor.Utility
       public static string GetDuration(DateTime startDateTime, DateTime endDateTime)
       {
          var duration = endDateTime - startDateTime;
-         if (duration.TotalDays > 2)
+         if (duration.TotalDays > 900)
+            return $"{Math.Round(duration.TotalDays / 365.25)} years";
+         if (duration.TotalDays > 35)
+            return $"{Math.Round(duration.TotalDays / 7)} weeks";
+         if (duration.TotalHours >= 48)
             return $"{Math.Round(duration.TotalDays)} days";
-         return duration.TotalHours > 2 
-            ? $"{Math.Round(duration.TotalHours)} hours" 
-            : $"{Math.Round(duration.TotalMinutes)} minutes";
+         if (duration.TotalMinutes >= 120)
+            return $"{Math.Round(duration.TotalHours)} hours";
+         
+         var minutes = (int) Math.Round(duration.TotalMinutes);
+         return minutes != 1
+            ? $"{minutes} minutes"
+            : $"{minutes} minute";
       }
    }
 }
