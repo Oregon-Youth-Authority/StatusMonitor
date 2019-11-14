32abdfa2c6e706fd6593a7cca5ee0768f440cfa0
Jeff Wobbe <wobbej@oya.state.or.us>
2019-11-14 12:30:19 -0800

    Added readme and build portable .exe zip

---
+++ StatusMonitor.Agent/Readme.txt
@@ -0,0 +1,5 @@
+﻿This application is used to send diagnostic connectivity data to the JJIS Team.  All data collected is visible here: https://oya-status-monitor.herokuapp.com/.
+
+Please refer to https://www.jjis.oregon.gov/staticcontent/jjisstatusmonitor/faq.html for any questions.
+
+The monitorSettings.json file is not required for this application to run, but can be used to override the computer and source IP.  The value of "Detect" in this file means that the system will try to detect the correct value.  You might want to consider overriding the source IP via configuration if your organization connects to JJIS via a VPN.  You might want to consider overriding the computer name via configuration if there is a value that would be more meaning full to your organization when viewing the data.
--- StatusMonitor.Agent/publish-signed-single-file.ps1
+++ StatusMonitor.Agent/publish-signed-single-file.ps1
@@ -11,3 +11,15 @@ dotnet-warp.exe $scriptPath -o $publishFilePath
 #For the following to work signtool must be in your path.  If not add it to you path environment variable.  Look for the exe under C:\Program Files (x86)\Windows Kits\10\
 #Also the Oregon Youth Authority Code Signing certificate must be in your certifificate store.  The certificate can be found in the developer keypass database
 signtool sign /n "Oregon Youth Authority" $publishFilePath
+
+copy-Item $scriptPath\Readme.txt $publishDir
+copy-Item $scriptPath\monitorSettings.json $publishDir
+
+#Create zip archive
+$archivePath = "$publishDir\JJIS.StatusMonitor.Agent.zip"
+$tempFile = New-TemporaryFile
+remove-item -force $tempFile
+remove-Item -force $archivePath
+add-type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::CreateFromDirectory($publishDir, $tempFile.FullName) 
+move-item -force $tempFile $archivePath
