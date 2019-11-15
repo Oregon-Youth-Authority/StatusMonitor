c7fb6110fdf00630ebdb52cf3dacb3c643f34995
Jeff Wobbe <wobbej@oya.state.or.us>
2019-11-14 16:34:52 -0800

    Added page to list by monitor name

---
+++ StatusMonitor.Server/Pages/ByMonitor.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationStatusMonitor.Abstractions;
+using ApplicationStatusMonitor.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ApplicationStatusMonitor.Pages
+{
+   public class ByMonitorModel : PageModel
+   {
+      private readonly IStatusRepository<StatusMonitorReply> _statusReplies;
+      private readonly ILogger<MonitorStatusModel> _logger;
+
+      public ByMonitorModel(IStatusRepository<StatusMonitorReply> statusReplies, ILogger<MonitorStatusModel> logger)
+      {
+         _statusReplies = statusReplies;
+         _logger = logger;
+      }
+
+      public IEnumerable<StatusMonitorReply> StatusReplies { get; private set; }
+
+      [BindProperty(SupportsGet = true)]
+      public string MonitorName { get; set; }
+
+      public void OnGet()
+      {
+         StatusReplies = _statusReplies.GetLatestStatusRecordForEachLocation().Where(x => x.MonitorName == MonitorName);
+      }
+
+   }
+}
