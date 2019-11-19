a91f4d165b8f66c56dee79c740ba4e0d04596358
Jeff Wobbe <wobbej@oya.state.or.us>
2019-11-19 13:54:44 -0800

    Separate online and offline monitors

---
+++ StatusMonitor.Server/Pages/Shared/_StatusListSplitPartial.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ApplicationStatusMonitor.Controllers;
+
+namespace ApplicationStatusMonitor.Pages.Shared
+{
+    public class StatusListSplitPartialModel : StatusListPartialModel
+    {
+       public IEnumerable<StatusMonitorReply> OnlineReplies => StatusMonitorReplies.Where(x => x.Status != "Offline");
+       public IEnumerable<StatusMonitorReply> OfflineReplies => StatusMonitorReplies.Where(x => x.Status == "Offline");
+
+
+      public void OnGet()
+        {
+
+        }
+    }
+}
